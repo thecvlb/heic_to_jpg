39b19ae41ddef9237f57aa2e6ce80c97af3a8e49
DimaOstrovskyiEk <dima.ostrovskiy@ekreative.com>
2023-07-19 18:35:17 +0300

    fix issues an assemble of a project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
+
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:heic_to_jpg/heic_to_jpg.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _path;
+  String? _path;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +25,7 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              RaisedButton(
+              ElevatedButton(
                 child: const Text('Pick a file...'),
                 onPressed: _pickFile,
               ),
@@ -32,7 +33,7 @@ class _MyAppState extends State<MyApp> {
                 padding: const EdgeInsets.only(top: 16.0),
                 child: _path != null
                     ? Image.file(
-                        File(_path),
+                        File(_path!),
                         width: 300.0,
                         height: 300.0,
                         fit: BoxFit.cover,
@@ -47,15 +48,12 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _pickFile() async {
-    final path = await FilePicker.getFilePath(
-        type: FileType.CUSTOM, fileExtension: "HEIC");
-    if (path != null) {
-      final jpgPath = await HeicToJpg.convert(path);
-      if (jpgPath != null) {
-        setState(() {
-          _path = jpgPath;
-        });
-      }
+    final result = await FilePicker.platform
+        .pickFiles(type: FileType.custom, allowedExtensions: ["heic"]);
+
+    if (result != null) {
+      final jpgPath = await HeicToJpg.convert(result.files.first.path!);
+      setState(() => _path = jpgPath);
     }
   }
 }
